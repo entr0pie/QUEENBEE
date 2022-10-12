61d1e18fea1cb2f0333b0a30000cfa56e75bb5a0
Caio Porcel <caiohporcel@gmail.com>
2022-10-12 08:52:21 -0300

    add 1000.py

---
+++ ex/1000/1000.py
@@ -0,0 +1,8 @@
+""" BEECROWN: https://github.com/entr0pie/QUEENBEE """
+# Date: 2022-10-11
+# Source: https://www.beecrowd.com.br/judge/en/problems/view/1000
+# Author: entr0pie
+# Contact: <https://github.com/entr0pie>
+# Name: BEE 1000
+
+print("Hello World!")
